648867800cf7616ef2e0216f3d62972d9483cd51
Tsvetan Stoyanov (I033024) <tsvetan.stoyanov@sap.com>
2015-12-21 19:36:13 +0200

    Wait for the remove VM before attempting to attach debugger. Fix for #11
    Change-Id: I55da563581b3f65b36ef5f399eeee5edb450530f

--- org.launchpi.launcher/src/org/launchpi/launcher/RPIDebugConfigurationDelegate.java
+++ org.launchpi.launcher/src/org/launchpi/launcher/RPIDebugConfigurationDelegate.java
@@ -1,15 +1,23 @@
 package org.launchpi.launcher;
 
 
+import java.io.IOException;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.IStreamListener;
+import org.eclipse.debug.core.model.IStreamMonitor;
 import org.eclipse.jdt.internal.launching.JavaRemoteApplicationLaunchConfigurationDelegate;
 import org.launchpi.launcher.i18n.Messages;
 
 @SuppressWarnings("restriction")
 public class RPIDebugConfigurationDelegate extends JavaRemoteApplicationLaunchConfigurationDelegate{
+	
+	private static final String DEBUG_READY_MESSAGE = "Listening for transport dt_socket at address:";
+	
+	private static final int REMOTE_VM_ATTACH_TIMEOUT = 30000;
 
 	@Override
 	public void launch(ILaunchConfiguration configuration, String mode,
@@ -17,13 +25,43 @@ public class RPIDebugConfigurationDelegate extends JavaRemoteApplicationLaunchCo
 
 		try {
 			RemoteProcess remoteProcess = new RemoteProcessFactory(launch, this, configuration, mode).createRemoteProcess(monitor);
+			DebugReadyListener debugReadyListener = new DebugReadyListener();
+			IStreamMonitor outputStreamMonitor = remoteProcess.getStreamsProxy().getOutputStreamMonitor();
+			outputStreamMonitor.addListener(debugReadyListener);
 			launch.addProcess(remoteProcess);
 			monitor.subTask(Messages.Progress_Waiting_Debug_Connection);
-			Thread.sleep(3000);
+			long maxTime = System.currentTimeMillis() + REMOTE_VM_ATTACH_TIMEOUT;
+			while (!remoteProcess.isTerminated()) {
+				if (System.currentTimeMillis() > maxTime) {
+					remoteProcess.terminate();
+					throw new IOException(Messages.Debug_Connection_Failed);
+				}
+				if (debugReadyListener.isRemoteVmReadyForDebug()) {
+					outputStreamMonitor.removeListener(debugReadyListener);
+					super.launch(configuration, mode, launch, monitor);
+					break;
+				}
+			}
 			monitor.worked(1);
-			super.launch(configuration, mode, launch, monitor);
 		} catch (Exception e) {
 			LaunchPlugin.reportError(Messages.Start_Failed, e);
 		}
+	}
+
+	private class DebugReadyListener implements IStreamListener {
+		
+		private boolean remoteVmReadyForDebug;
+		
+		public boolean isRemoteVmReadyForDebug() {
+			return remoteVmReadyForDebug;
+		}
+
+		@Override
+		public void streamAppended(String text, IStreamMonitor monitor) {
+			if (!remoteVmReadyForDebug && text.startsWith(DEBUG_READY_MESSAGE)) {
+				remoteVmReadyForDebug = true;
+			}
+		}
+		
 	}
 }
--- org.launchpi.launcher/src/org/launchpi/launcher/i18n/Messages.java
+++ org.launchpi.launcher/src/org/launchpi/launcher/i18n/Messages.java
@@ -22,6 +22,7 @@ public class Messages extends NLS {
 	public static String Progress_Launching_Java;
 	public static String Progress_Synchronizing_CP;
 	public static String Progress_Waiting_Debug_Connection;
+	public static String Debug_Connection_Failed;
 	public static String Raspberry_PI;
 	public static String Run_As_Root;
 	static {
